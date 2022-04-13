9039e0a8190fba2a2413a1825b009091c2c69dea
Reginald Arnedo <reginald.arnedo@gmail.com>
2022-04-12 20:31:21 -0700

    Pushing updated View/Component and working with Kai to fix

---
+++ vue/src/components/ParentDashboard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="parent-dashboard">
+      <ul id="banner">
+      <li>FAQ</li>
+      </ul>
+    <h1 id="title">Parent Dashboard</h1>
+    <h3 id="subtext">Keep track of your kid's reading. Build habits for life.</h3>
+      <table class="Children List">
+      <label for="Children">Children **TABLE OF CHILDREN ACCOUNTS**</label>
+        <!-- <tr>
+          <th>Name</th>
+          <th>Books Finished</th>
+          <th>Hours Read</th>
+          <th>Last Active?</th>
+        </tr>-->
+      </table> 
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Create | Edit Child Account IN PROGRESS/WIP
+      </button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Create | Edit Parent Account
+      </button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Logout
+      </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/ParentDashboardView.vue
+++ vue/src/views/ParentDashboardView.vue
@@ -1,38 +1,14 @@
 <template>
-  <div class="parent-dashboard">
-      <ul id="banner">
-      <li>FAQ</li>
-    </ul>
-    <h1 id="title">Parent Dashboard</h1>
-    <h3 id= "subtext">Keep track of your kid's reading. Build habits for life.</h3>
-      <table class="Children List">
-      <label for="Children">Children **TABLE OF CHILDREN ACCOUNTS**</label>
-        <!-- <tr>
-          <th>Name</th>
-          <th>Books Finished</th>
-          <th>Hours Read</th>
-          <th>Last Active?</th>
-        </tr>-->
-      </table> 
-
-    <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create | Edit Child Account IN PROGRESS/WIP
-      </button>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create | Edit Parent Account
-      </button>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Logout
-      </button>
-
-  </div>
+  <parent-dashboard />
 </template>
 
 <script>
-// import authService from "@/services/AuthService";
+import ParentDashboard from '@/components/ParentDashboard'
 
 export default {
-
+  components: {
+    ParentDashboard
+  },  
 }
 </script>
 
