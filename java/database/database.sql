b37f4219c27eeb07a559880c970914dad5dbd811
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2022-04-14 12:06:49 -0500

    DB changes as per current sprint backlog requirements

--- java/database/database.sql
+++ java/database/database.sql
@@ -14,8 +14,8 @@ CREATE TABLE book_info (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     author VARCHAR(100) NOT NULL,
-	isbn int
-	
+	isbn BIGINT
+
 );
 
 CREATE TABLE users (
@@ -23,7 +23,7 @@ CREATE TABLE users (
 	family_id varchar(255) NOT NULL,
 	first_name varchar(50) NOT NULL,
 	last_name varchar(50) NOT NULL,
-	email varchar(255) NOT NULL,
+	email varchar(255),
 	username varchar(50) UNIQUE NOT NULL,
 	password_hash varchar(255) NOT NULL,
 	role varchar(50) NOT NULL,
@@ -33,60 +33,52 @@ CREATE TABLE users (
 CREATE TABLE users_books (
 	user_id int NOT NULL,
 	book_id int NOT NULL,
+	reading_format varchar (50) NOT NULL,
 	times_read int,
 	past_book boolean,
 	current_book boolean,
 	future_book boolean,
+	notes varchar (50),
 	CONSTRAINT PK_users_books PRIMARY KEY(user_id, book_id),
     FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(book_id) REFERENCES book_info(book_id)
-	
+
 );
 
-INSERT INTO users (username, family_id, first_name, last_name, email, password_hash, role) 
-VALUES 
+INSERT INTO users (username, family_id, first_name, last_name, email, password_hash, role)
+VALUES
 	('user', '1', 'Colin', 'Davis', 'cd@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'),
 	('admin', '1', 'Nolan', 'Tsai', 'nt@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN'),
 	('username', '2', 'Reg', 'Arnedo', 'reg@hotmail.com', 'testpasshash', 'ROLE_USER'),
 	('username1', '2', 'Kai', 'Wolf', 'Kai@hotmail.com', 'passwordhash', 'ROLE_USER'),
 	('dolphinnet', '3', 'Bill', 'Gates', 'bill@outlook.com', 'microsoft', 'ROLE_ADMIN'),
-	('deepdive', '3', 'Feng-hsiung', 'Hsu', 'Hsu@fenghsi.ung', 'taiwan', 'ROLE_USER'),
-	('robotbill', '3', 'William', 'Normal', 'bill@gmail.com', 'passwordbill', 'ROLE_USER'),
-	('elevatorman', '4', 'Carlos', 'Bootcamp', 'carlos@techelevator.com', 'elevator', 'ROLE_USER'),
-	('notnull', '4', 'Joe', 'Null', 'joe@nu.ll', 'fakedata', 'ROLE_USER'),
-	('realname', '4', 'Real', 'Name', 'realemail@hotmail.com', 'realpass', 'ROLE_ADMIN'),
-	('KingJames', '5', 'Lebron', 'James', 'Lebron@nba.org', 'jamesjames', 'ROLE_USER'),
-	('LBird', '5', 'Larry', 'Bird', 'Larry@mlb.org', 'birdman', 'ROLE_USER');
+	('deepdive', '7', 'Feng-hsiung', 'Hsu', 'blah@gmail.com', 'taiwan', 'ROLE_USER');
 
 INSERT INTO book_info (title, author, isbn)
 VALUES
-    ('Book Title 1', 'Colin Davis', 12345),
-    ('Book Title 2', 'Nolan Tsai', 23456),
-	('Book Title 3', 'Reginald Arnedo', 34567),
-	('Book Title 4', 'Kai Indigo Wolf', 45678),
-	('How to Play Your Best Golf All the Time', 'Tommy Armour', 164164),
-	('The Rainbow Fish', 'J Alison James', 13722),
-	('Just Me and My Dad', 'Mercer Mayer', 11911),
-	('If You Give a Mouse a Cookie', 'Felicia Bond', 9939),
-	('Where the Wild Things Are', 'Maurice Sendak', 29404),
-	('Brown Bear, Brown Bear, What Do You See?', 'Brown Bear', 39572),
-	('The Very Hungry Caterpillar', 'Reginald Arnedo', 9027),
-	('Strictly No Elephants', 'Taeeun Yoo', 3106);
+    ('The Super Cool Life of a Tech Lead', 'Colin Davis', 1234216543535),
+    ('What is For Dinner?', 'Nolan Tsai', 2348734681156),
+	('Project Managing, Like a Boss', 'Reginald Arnedo', 3458130428567),
+	('Just Let me Make it Pretty', 'Kai Indigo Wolf', 4568142953178),
+	('How to Play Your Best Golf All the Time', 'Tommy Armour', 1640569132164),
+	('The Rainbow Fish', 'J Alison James', 1379158253022),
+	('Just Me and My Dad', 'Mercer Mayer', 1191739256711),
+	('If You Give a Mouse a Cookie', 'Felicia Bond', 99017989234539),
+	('Where the Wild Things Are', 'Maurice Sendak', 2901623865404),
+	('Brown Bear, Brown Bear, What Do You See?', 'Brown Bear', 3987123096572),
+	('The Very Hungry Caterpillar', 'Reginald Arnedo', 9009890674127),
+	('Strictly No Elephants', 'Taeeun Yoo', 3116237654306);
 
-INSERT INTO users_books (user_id, book_id, times_read, past_book, current_book, future_book) 
-VALUES 
-	(1, 1, 1, true, false, false),
-	(1, 2, 0, false, false, true),
-	(2, 1, 3, true, true, false),
-	(2, 3, 0, false, true, false),
-	(3, 3, 0, false, false, true),
-	(4, 3, 5, true, false, false),
-	(4, 8, 0, false, false, true),
-	(5, 9, 0, false, true, false),
-	(5, 11, 1, true, true, false),
-	(6, 2, 0, false, false, true),
-	(7, 5, 0, false, true, false),
-	(8, 9, 2, true, false, false);
+INSERT INTO users_books (user_id, book_id, reading_format, times_read, past_book, current_book, future_book, notes)
+VALUES
+	(1, 1, '', 2, true, false, false, 'Definitely good for sleepy time.'),
+	(1, 2, '', 1, false, false, true, ''),
+	(2, 1, '', 1, true, true, false, 'Colin said this one is too wordy.'),
+	(2, 3, '', 2, false, true, false, ''),
+	(3, 3, '', 3, false, false, true, ''),
+	(4, 3, '', 1, true, false, false, ''),
+	(4, 8, '', 1, false, false, true, ''),
+	(5, 9, '', 5, false, true, false, 'Reg loves this one! He asks for it every night.');
 
 --- USER SETUP (Do Not Modify)
 DROP USER IF EXISTS final_capstone_owner;
