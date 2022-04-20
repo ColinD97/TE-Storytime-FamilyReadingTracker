298f765e2dba190202d39dfdff57c1c06bd29085
Colin <crdavis1997@yahoo.com>
2022-04-20 11:21:26 -0500

    addBook submit successful on vue side

--- vue/src/components/AddBook.vue
+++ vue/src/components/AddBook.vue
@@ -1,73 +1,69 @@
 <template>
-    
-<div>
-
-  <form class="form-add-book">
-    <h1 class="form-header">Add A Book To Family Collection</h1>
-    <div class="form-body">
-            <!-- Title and Author -->
-            <div class="horizontal-group">
-                <div class="form-group left">
-                    <label for="title" class="label-title">Book Title</label>
-                    <input
-                        type="text"
-                        id="title"
-                        class="form-input"
-                        placeholder=""
-                        v-model="book.title"
-                        required
-                        autofocus
-                    />
-                    </div>
-                <div class="form-group right">
-                    <label for="author" class="label-title">Author</label>
-                    <input
-                        type="text"
-                        id="author"
-                        class="form-input"
-                        placeholder=""
-                        v-model="book.author"
-                        autofocus
-                    />
-                    </div>
-            </div>
-            <!-- ISBN and Difficulty-->
-            <div class="horizontal-group">
-                    <div class="form-group left">
-                        <label for="email" class="label-title">ISBN</label>
-                        <input
-                            type="text"
-                            id="isbn"
-                            class="form-input"
-                            placeholder=""
-                            v-model="book.isbn"
-                            autofocus
-                        />
-                    </div>
-                    <div class="form-group right">
-                    <label for="difficulty" class="label-title">Difficulty</label>
-                    <select
-                        class="drop-down"
-                        name="difficulty"
-                        id="difficulty"
-                        v-model="book.difficulty"
-                        autofocus
-                    >
-                      <option disabled hidden value="">Pick Difficulty</option>
-                      <option selected value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
-                    </div>
-                </div>
-            </div>
-              <div class="form-footer">
-                <button class="btn" type="submit" v-on:click="addBook">
-                Add Book
-                </button>
-                  <!-- <div class="custom-select" style="width:350px;" id='dropdown'>
+  <div>
+    <form class="form-add-book">
+      <h1 class="form-header">Add A Book To Family Collection</h1>
+      <div class="form-body">
+        <!-- Title and Author -->
+        <div class="horizontal-group">
+          <div class="form-group left">
+            <label for="title" class="label-title">Book Title</label>
+            <input
+              type="text"
+              id="title"
+              class="form-input"
+              placeholder=""
+              v-model="book.title"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-group right">
+            <label for="author" class="label-title">Author</label>
+            <input
+              type="text"
+              id="author"
+              class="form-input"
+              placeholder=""
+              v-model="book.author"
+              autofocus
+            />
+          </div>
+        </div>
+        <!-- ISBN and Difficulty-->
+        <div class="horizontal-group">
+          <div class="form-group left">
+            <label for="email" class="label-title">ISBN</label>
+            <input
+              type="text"
+              id="isbn"
+              class="form-input"
+              placeholder=""
+              v-model="book.isbn"
+              autofocus
+            />
+          </div>
+          <div class="form-group right">
+            <label for="difficulty" class="label-title">Difficulty</label>
+            <select
+              class="drop-down"
+              name="difficulty"
+              id="difficulty"
+              v-model="book.difficulty"
+              autofocus
+            >
+              <option disabled hidden value="">Pick Difficulty</option>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="form-footer">
+        <button class="btn" type="submit" v-on:click="addBook">Add Book</button>
+        <!-- <div class="custom-select" style="width:350px;" id='dropdown'>
                     <label for="custom-select">Assign book to:</label>
                     <select 
                     class="drop-down"
@@ -78,9 +74,9 @@
                       <option v-for="user in familyUsers" v-bind:key="user.index" :value="user.id">{{user.first_name}}</option>
                     </select>
                   </div>   -->
-                </div> 
-  </form>
-</div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -100,36 +96,51 @@ export default {
         difficulty: "",
         genre: "",
       },
-      assignToUser: "",
+      assignToUser: 1,
       isbnErrors: false,
       isbnErrorMsg: 'ISBN needs to be a 13 digit number.',
       
     };
   },
   methods: {
+    // addBook() {
+    //   if (this.book.isbn !== (/([0-9]){13}/g)) {
+    //     this.isbnErrors = true;
+    //     this.isbnErrorMsg = 'ISBN needs to be a 13 digit number.';
+    //   } else {
+    //   console.log('add book'+ this.book)
+    //   BookService
+    //   // Assign to family ID
+    //     .addBook(this.assignToUser, this.book)
+    //     .then(response => {
+    //       if (response.status === 201) {              
+    //           this.book = {
+    //             title: '',
+    //             author: '',
+    //             isbn: '',
+    //             difficulty: '',
+    //             genre: '',       
+    //           },
+    //         alert('AddBook successful!  Hurray for our side!'),
+    //         this.$router.push({path: '/'});
+    //       }
+    //     })
+    //   }
+    // }
     addBook() {
-      if (this.book.isbn !== (/([0-9]){13}/g)) {
-        this.isbnErrors = true;
-        this.isbnErrorMsg = 'ISBN needs to be a 13 digit number.';
-      } else {
-      console.log('add book'+ this.book)
-      BookService
-      // Assign to family ID
-        .addBook(this.assignToUser, this.book)
-        .then(response => {
-          if (response.status === 201) {              
-              this.book = {
-                title: '',
-                author: '',
-                isbn: '',
-                difficulty: '',
-                genre: '',       
-              },
-            alert('AddBook successful!  Hurray for our side!'),
-            this.$router.push({path: '/'});
-          }
-        })
-      }
+      BookService.addBook(this.assignToUser, this.book)
+      .then(response => {
+        if (response.status === 201) {              
+          this.book = {
+            title: '',
+            author: '',
+            isbn: '',
+            difficulty: '',
+            genre: '',       
+          },
+          alert('AddBook successful!  Hurray for our side!')
+        }
+      })
     }
   }
 }
@@ -142,19 +153,17 @@ export default {
 /* Buttons */
 /*---------------------------------------*/
 .btn-body {
-    display: inline-block;
-    padding: 10px 10px;
-    background-color: #1ca3dc;
-    font-size: 17px;
-    border: none;
-    border-radius: 5px;
-    color: #bcf5e7;
-    cursor: pointer;
+  display: inline-block;
+  padding: 10px 10px;
+  background-color: #1ca3dc;
+  font-size: 17px;
+  border: none;
+  border-radius: 5px;
+  color: #bcf5e7;
+  cursor: pointer;
 }
 .btn-body:hover {
-    background-color: #157788;
-    color: white;
+  background-color: #157788;
+  color: white;
 }
-
-
 </style>
